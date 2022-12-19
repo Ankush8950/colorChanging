2fca4ed285f2573e26ba093c9acddb3415838fcb
Ankush8950 <69650280+Ankush8950@users.noreply.github.com>
2022-12-19 23:44:01 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const button = document.getElementById("button")
 const canvas = document.getElementById("canvas")
+
+
 let color = ["#DE4839", "#0D0D0D", "#00D84A", "#120E43", "#50DBB4", "#758283", "#5A20CB"];
 
+// click event
 button.addEventListener("click",function(){
   const  randomcolor = color[Math.floor(Math.random()*color.length)] 
   canvas.style.backgroundColor = randomcolor
